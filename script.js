92bc3723df18bfa0e5a13359ad5eb8b2452c96a1
Pablo Silva <120851304+FrkP9@users.noreply.github.com>
2024-04-16 07:42:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ function ligar() {
     if (op == 0) {
         document.getElementById("lamp").src = lampOn;
         document.getElementById("aLig").play();
-        document.body.style.backgroundImage = "url('/img/bg-luz-on.jpg')";
+        document.body.style.backgroundImage = "url('img/bg-luz-on.jpg')";
         document.getElementById("title").style.color = "black";
         op = 1;
     } else {
         document.getElementById("lamp").src = lampOff;
         document.getElementById("aDeslig").play();
-        document.body.style.backgroundImage = "url('/img/bg-luz-off.jpg')";
+        document.body.style.backgroundImage = "url('img/bg-luz-off.jpg')";
         document.getElementById("title").style.color = "#06C1DE";
 
         op = 0;
